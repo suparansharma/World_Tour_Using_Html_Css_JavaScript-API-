87c47ec1b0375ebb1d1035e95b12c0b9996972e1
suparansharma <49103115+suparansharma@users.noreply.github.com>
2021-07-02 18:20:40 +0600

    show all countries capaital name

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ fetch('https://restcountries.eu/rest/v2/all')
 .then(data => displayCountries(data));
 
 const displayCountries = countries =>{
-    const ul = document.getElementById('countries');
+    const countriesDiv = document.getElementById('countries');
     for (let i = 0; i < countries.length; i++) {
         const country = countries[i];
-        const li = document.createElement('li');
-        li.innerText = country.name;
-        ul.appendChild(li);
+        const countryDiv = document.createElement('div');
+
+        const h3 = document.createElement('h3');
+        h3.innerText = country.name;
+
+        const p = document.createElement('p');
+        p.innerText = country.capital;
+
+        countryDiv.appendChild(h3);
+        countryDiv.appendChild(p);
+        countriesDiv.appendChild(countryDiv);
         
     }
 }
